b83dc925772151d284f24ca88fe8fa778fc3f50e
vimcy7 <vimcy7@gmail.com>
2022-10-02 15:16:13 +0530

    Added source code for A4

---
+++ assignments/04_micro_to_miniRA/RollNo_P4/P4/P4.java
@@ -0,0 +1,18 @@
+import syntaxtree.*;
+import visitor.*;
+
+public class P4 {
+   public static void main(String [] args) {
+      try {
+         Node root = new microIRParser(System.in).Goal();
+         //System.out.println("Program parsed successfully");
+         Object value = root.accept(new GJDepthFirst(), null); // Your assignment part is invoked here.
+      }
+      catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+} 
+
+
+
